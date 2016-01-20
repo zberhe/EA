6300188e38e1b953b6d84205f0664a07c021b930
Zberhe <zehaftatekea@gmail.com>
2016-01-20 00:01:37 -0600

    Autowiring by Constructor - Constructor Injection

---
+++ DependenceInjection/AutoWiringByConstructor/src/main/java/cs544/AutoWiringByConstructor/SpellChecker.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs544.AutoWiringByConstructor;
+
+/**
+ *
+ * @author Zehafta M
+ */
+public class SpellChecker {
+
+    public SpellChecker() {
+        System.out.println("Inside SpellChecker constructor.");
+    }
+
+    public void checkSpelling() {
+        System.out.println("Inside checkSpelling.");
+    }
+
+}
